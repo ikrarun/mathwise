b8fbe6bb76fad626218807bf62a1f87ba920203e
ikrarun <iamkrarun@gmail.com>
2024-03-14 00:25:52 +0530

    ee

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    missingSuspenseWithCSRBailout: false,
     serverActions: {
       allowedOrigins: [
         "fuzzy-fortnight-qwjj7495qrwh4gw9-3000.app.github.dev/" /* or Codespace port forward url, no including scheme */,
