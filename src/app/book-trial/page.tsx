091729655a42624e68352f3cbd2562e9cf078ef4
ikrarun <iamkrarun@gmail.com>
2024-03-13 11:31:25 +0000

    form Done Right

--- src/app/book-trial/page.tsx
+++ src/app/book-trial/page.tsx
@@ -1,20 +1,26 @@
 'use client'
 import React from 'react'
 import Script from 'next/script'
-
+import { delaGothic as font } from './../../fontList'
 const Page = () => {
-
     return (
-        <main className=" w-full min-h-full flex-grow flex-col flex px-6 pt-16 py-3 items-center justify-start gap-6 max-w-[900px] ">
-            <iframe data-tally-src="https://tally.so/embed/3EXKQl?alignLeft=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="216" frameBorder={0} marginHeight={0} marginWidth={0} title="Book Your Free Trial"></iframe>
+        <main className=" w-full relative min-h-full flex-grow flex-col flex px-6 pt-16 py-3 items-center justify-start gap-6 max-w-[900px] ">
+
+            <div id='frame' className=' w-full z-50 '>
+                <iframe data-tally-src="https://tally.so/embed/3EXKQl?alignLeft=1&dynamicHeight=1" loading="lazy" width="100%" height="240" frameBorder={0} marginHeight={0} marginWidth={0} title="Book Your Free Trial"></iframe>
+            </div>
             <Script
                 id="tally-js"
                 src="https://tally.so/widgets/embed.js"
                 onLoad={() => {
                     // @ts-ignore
                     Tally.loadEmbeds();
+
                 }}
             />
+            <h1 style={font.style} className='z-10 absolute self-center animate-pulse duration-200 top-24 text-6xl ease-in-out' role="status" id='loader'>
+                Please Wait ....
+            </h1>
         </main>
     )
 }
--- src/app/book-trial/serveraction.ts
+++
@@ -1,5 +0,0 @@
-"use server";
-
-export default async function serveraction(data: FormData) {
-  console.log(data);
-}
